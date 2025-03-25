77845db7bd67e282929c46533cc0405670ba30c1
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-03-24 23:53:13 -0300

    create Steps.css

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,17 +1,21 @@
-import { AIOutLineUser, AiOutLineStar } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineStar } from "react-icons/ai";
 
 import "./Steps.css";
+import { FiSend } from "react-icons/fi";
 
 const Steps = ({ currentStep }) => {
   return (
     <div className="steps">
-      <div className="step">
+      <div className="step active">
+        <AiOutlineUser />
         <p>Identificação</p>
       </div>
       <div className="step">
+        <AiOutlineStar />
         <p>Avaliação</p>
       </div>
       <div className="step">
+        <FiSend />
         <p>Envio</p>
       </div>
     </div>
